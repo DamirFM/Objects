203b0cdf4bf0ab92791463db04f972040b8328a6
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-04 23:19:06 -0400

    add path

---
+++ lessons/path.js
@@ -0,0 +1,12 @@
+// accepting the site name as input and returning information about it:
+const getDomainInfo = (file) => {
+    let scheme = '';
+    if (file.startsWith('https://')) {
+      scheme = 'https';
+    } else {
+      scheme = 'http';
+    }
+    const name = file.replace(`${scheme}://`, '');
+    return {scheme, name};
+  };
+  export default getDomainInfo;
