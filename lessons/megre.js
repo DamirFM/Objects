6d745b17ae1a6da2b0469351b76673ff75ebd468
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-15 20:05:10 -0400

    add merge.js

--- lessons/megre.js
+++ lessons/megre.js
@@ -1,4 +1,22 @@
-const merge = (object, keys, data) => {
-    const filteredData = keys.length > 0 ? _.pick(data, keys) : data;
-    Object.assign(object, filteredData);
-  };
+// Implement and export by default a function 
+// that populates an object with data from another object
+//  based on a permitted list of keys. Options:
+
+// Source object:
+// List of keys to be replaced
+// Data to be merged into the source object
+// In the case when the list of keys is empty, you need to merge all the data completely.
+
+// I used the helper function _.pick from the lodash library
+
+
+const object = (company, array, data) => {
+  let filtred = {};
+  if (array.length > 0) {
+    filtred = _.pick(data, array);
+  } else {
+    filtred = data;
+  }
+  Object.assign(company, filtred); 
+};
+export default object;
