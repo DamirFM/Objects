caa4dd921c6b5247586972cfd8a5029df411c86a
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-16 23:16:55 -0400

    add update clonedeep.js

--- lessons/cloneDeep.js
+++ lessons/cloneDeep.js
@@ -1,1 +1,18 @@
 // I have implemented a function that does a deep copy of objects.
+// For the solution, I need to sequentially traverse the original 
+// object and copy its data to another object. 
+// If the value of some property is an object, 
+// then you need to recursively run the implemented function.
+
+const cloneDeep = (data) => {
+
+    const keys = Object.keys(data);
+    for (const [key] of keys) {
+      const current = Object.assign({}, data);
+      console.log(current);
+      if (_.isObject(key)) {
+        Object.entries(obj)
+      }
+    }
+  
+  };
