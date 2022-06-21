5b3ecad66e7f2b7e06f0c427046494248e2ac837
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-20 23:40:17 -0400

    add spread.js

--- lessons/spread.js
+++ lessons/spread.js
@@ -2,3 +2,15 @@
 // a variable (or constant) name, inside an object definition. 
 // It lays out the corresponding object inside a new object.
 //  It can only get a copy, it cannot modify existing objects.
+
+// An implementation of a function that creates a company object and returns it. 
+// To create a company, only one property is required - the name of the company. 
+// Other properties are added only if they exist. Options:
+
+// Name
+// Object with additional properties
+// Companies also have two properties with default values:
+
+// state - moderating
+// createdAt is the current date (in Unix time format. 
+// This number is the number of milliseconds that have passed since midnight, January 1, 1970)
