ce9a3356bfb42a83740b5dfba9be48de282a2a0f
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-20 23:29:15 -0400

    add spread.js

---
+++ lessons/spread.js
@@ -0,0 +1,4 @@
+// A spread statement is three dots before 
+// a variable (or constant) name, inside an object definition. 
+// It lays out the corresponding object inside a new object.
+//  It can only get a copy, it cannot modify existing objects.
