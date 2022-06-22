7d26a8a87a7782477d687a8c169a2c358ac0e2c4
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-21 23:28:13 -0400

    add Modifocation.js

--- lessons/Modification.js
+++ lessons/Modification.js
@@ -1,4 +1,5 @@
-
+// A function that "normalizes" the data of the passed lesson. 
+// That is, it leads them to a certain form. Normalization occurs by changing the original object.
 
 const normalize = (lesson) => {
     lesson.name = _.capitalize(lesson.name);
