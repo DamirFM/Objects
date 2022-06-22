4a536111e7b677add72e8a1ee4dafc611fab7318
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-21 23:36:22 -0400

    add count-words.js

--- lessons/count-words.js
+++ lessons/count-words.js
@@ -1,3 +1,9 @@
+// A function that counts the number of words in a sentence, 
+// and returns an object. In the object, the properties are the words 
+// (reduced to lowercase), and the values are the number of times the word occurs in the sentence. 
+// Words in a sentence can be in different registers. 
+// Before counting, they must be reduced to lowercase so that duplicates are not skipped.
+
 import _ from 'lodash';
 
 // BEGIN (write your solution here)
