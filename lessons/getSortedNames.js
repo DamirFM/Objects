f0b676fb3147aa5310aead34c4b03ed631e6c64d
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-21 23:46:56 -0400

    getSortedNames.js

--- lessons/getSortedNames.js
+++ lessons/getSortedNames.js
@@ -1,2 +1,13 @@
 // A function that takes a list of users as input, 
 // extracts their names, sorts them alphabetically, and returns a sorted list of names.
+
+const mainList =    [
+    { name: 'Bronn', gender: 'male', birthday: '1973-03-23' },
+    { name: 'Reigar', gender: 'male', birthday: '1973-11-03' },
+    { name: 'Eiegon', gender: 'male', birthday: '1963-11-03' },
+    { name: 'Sansa', gender: 'female', birthday: '2012-11-03' }
+  ];
+
+  const getSortedNames = (mainList) => {
+    console.log(mainList);
+    };
