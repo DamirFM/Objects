c5ce52de238c9c4a8ca407816c59169be4ab4713
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-21 23:30:26 -0400

    add data-matches.js

--- lessons/data-matches.js
+++ lessons/data-matches.js
@@ -1,3 +1,8 @@
+// A function that compares objects by matching data, not by reference. 
+// This function takes two companies as input and returns true 
+// if their structure is the same, and false otherwise. 
+// The check should go through the properties name, state, website.
+
 export default (company1, company2) => {
     const keys = ['name', 'state', 'website'];
     for (const key of keys) {
