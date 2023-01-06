6984d79a4c75ad8ad4818b05bfb246eacda01868
Damir Yakupov <yakupovdamir93@gmail.com>
2023-01-05 23:23:21 -0500

    add updated function inside

--- lessons/Modification.js
+++ lessons/Modification.js
@@ -1,9 +1,7 @@
 // A function that "normalizes" the data of the passed lesson. 
 // That is, it leads them to a certain form. Normalization occurs by changing the original object.
 
-const normalize = (lesson) => {
-    lesson.name = _.capitalize(lesson.name);
-    lesson.description = lesson.description.toLowerCase();
-  };
-  
-  export default normalize;
+export default function normalize(lesson) {
+  lesson.name = _.capitalize(lesson.name);
+  lesson.description = lesson.description.toLowerCase();
+}
