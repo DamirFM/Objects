59eb73d0f64f31b216372cbb2f59f747b2bade1d
Damir Yakupov <yakupovdamir93@gmail.com>
2023-01-13 09:27:47 -0500

    add one more example of Object.hasOwn method

---
+++ lessons/Object.hasOwn1.js
@@ -0,0 +1,29 @@
+// Implement and export a default function 
+// that transforms the passed object into another 
+// object that includes only the specified properties. Options:
+// Source object
+// Array of property names
+
+// const array = ['none', 'cores'];
+
+// const obj = {
+//    user: 'ubuntu',
+//    cores: 4,
+//    os: 'linux',
+//  };
+
+// BEGIN (write your solution here)
+export default function pick(obj, array) {
+ // In this step will create the object
+    const result = {};
+    for (const key of array) {
+    // here we will enumerate elements and check 
+    // if existing object has the indicated property 
+      if (Object.hasOwn(obj, key)) {
+        result[key] = obj[key];
+      }
+    }
+    
+    return result;
+  }
+  // END
