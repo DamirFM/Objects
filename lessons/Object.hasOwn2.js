d69a6db3d9cad69f8391406c0c330d48864a9d00
Damir Yakupov <yakupovdamir93@gmail.com>
2023-01-17 20:04:49 -0500

    add one more sample of Object.hasOwn method

---
+++ lessons/Object.hasOwn2.js
@@ -0,0 +1,46 @@
+// The implementation of a function 
+// that extracts a value from an object 
+// of any nesting depth by the specified keys. Options:
+
+//Source object:
+// Keychain (array) to search for the value
+// In the case when it is impossible to get to the value, null is returned.
+
+const data = {
+    user: 'ubuntu',
+    hosts: {
+      0: {
+        name: 'web1',
+      },
+      1: {
+        name: 'web2',
+        null: 3,
+        active: false,
+      },
+    },
+  };
+
+
+  const keys = ['hosts', 1, 'active'];
+
+// BEGIN (write your solution here)
+export default function get (obj, keys) {
+// В процессе работы мы должны постепенно заходить вглубь объекта
+// и проверять наличие ключа на текущем уровне вложенности.
+// Переменная current хранит содержимое объекта на текущем уровне вложенности.
+// Использование переменной позволяет нам не менять параметр функции, 
+// так как это является плохой практикой
+  let current = obj;
+  for (const key of keys) {
+    const check = Object.hasOwn(current, key);
+    if (!check) {
+      return null;
+    }
+  // B current хранится переданный объект. 
+  // Этой строчкой мы перезаписываем эту переменную, 
+  // передавая туда значение объекта под ключом key
+    current = current[key]
+  }
+  return current;
+}
+// END
