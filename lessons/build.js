89d5db0128d9181607f5f3544af8f1e09097649a
Damir Yakupov <yakupovdamir93@gmail.com>
2023-02-06 23:54:00 -0500

    add build.js

--- lessons/build.js
+++ lessons/build.js
@@ -6,9 +6,12 @@ export default function build(data) {
     const entries = Object.entries(data);
   
     for (const [key, value] of entries) {
-      
-      console.log(key);
-      console.log(value)
+      const name1 = key;
+      const name2 = value;
+      const result = `${name1}=${name2}`
+      console.log(result)
+      return result;
+     
     }
     if (data) {
       return ''
