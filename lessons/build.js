76d055f098fcee22ceec213e4c0ac5f41e5c4d6c
Damir Yakupov <yakupovdamir93@gmail.com>
2023-02-07 00:09:22 -0500

    add build

--- lessons/build.js
+++ lessons/build.js
@@ -2,19 +2,13 @@
 
 // BEGIN (write your solution here)
 export default function build(data) {
- 
+    const total = [];
     const entries = Object.entries(data);
-  
     for (const [key, value] of entries) {
-      const name1 = key;
-      const name2 = value;
-      const result = `${name1}=${name2}`
-      console.log(result)
-      return result;
-     
+      const result = `${key}=${value}`;
+      total.push(result);
     }
-    if (data) {
-      return ''
-    }
+    const sorted = total.sort()
+    return sorted.join('&')
   }
   // END
