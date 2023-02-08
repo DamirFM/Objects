6ed86cb3003286d4f41a89daf6b749970c1aa86a
Damir Yakupov <yakupovdamir93@gmail.com>
2023-02-07 22:09:56 -0500

    add build.js

--- lessons/build.js
+++ lessons/build.js
@@ -1,14 +1,24 @@
+// Реализуйте и экспортируйте функцию по умолчанию, 
+// которая принимает на вход список параметров 
+// и возвращает сформированный query string из этих параметров:
 
+
+ 
+// bqs({ per: 10, page: 1 });
+// page=1&per=10
+// bqs({ param: 'all', param1: true });
+// param=all&param1=true
+// Имена параметров в выходной строке должны располагаться 
+// в алфавитном порядке (то есть их нужно отсортировать).
 
 // BEGIN (write your solution here)
 export default function build(data) {
     const total = [];
     const entries = Object.entries(data);
-    for (const [key, value] of entries) {
-      const result = `${key}=${value}`;
-      total.push(result);
+    const sortedEntries = entries.sort();
+    for (const [key, value] of sortedEntries) {
+      total.push(`${key}=${value}`);
     }
-    const sorted = total.sort()
-    return sorted.join('&')
+    return total.join('&');
   }
   // END
