c32cc9bd845e8d2f007e4b5e94bfa145fde51b3a
Damir Yakupov <yakupovdamir93@gmail.com>
2023-02-09 00:38:26 -0500

    add more

--- lessons/ObjectCompare.js
+++ lessons/ObjectCompare.js
@@ -34,3 +34,44 @@ export default function genDiff(firstObj, secondObj) {
 }
 
 // END
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
+import _ from 'lodash';
+
+// BEGIN (write your solution here)
+export default function genDiff(firstObj, secondObj) {
+  
+  const thirdObj = Object.assign(firstObj, secondObj);
+  let current = thirdObj;
+  const keys = Object.keys(thirdObj);
+  const uni = _.union(keys);
+  for (const key of uni) {
+    const check1 = Object.hasOwn(firstObj, key);
+    const check2 = Object.hasOwn(secondObj, key);
+    
+    
+
+    if (check2 && check1) {
+      current[key] = 'added';
+    }
+    else if (check1 && !check2) {
+      current[key] = 'deleted';
+    }
+
+  
+
+    
+
+    
+
+  }
+  
+  console.log(current)
+  return current;
+
+
+  
+}
+
+// END
