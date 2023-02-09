0af7662df34abeb946d96d1c5cbd28630c2ddf3e
Damir Yakupov <yakupovdamir93@gmail.com>
2023-02-08 22:33:34 -0500

    add One more step

--- lessons/ObjectCompare.js
+++ lessons/ObjectCompare.js
@@ -1,24 +1,27 @@
 
 
+
+import _ from 'lodash';
+
 // BEGIN (write your solution here)
 export default function genDiff(firstObj, secondObj) {
-    const thirdObj = Object.assign(secondObj, firstObj); 
-    const keys = Object.keys(thirdObj);
-    console.log(thirdObj.one)
-    for (const key of keys) {
-      
-      const hasProperty = Object.hasOwn(secondObj, key); 
-      if (!hasProperty) {
-        
-      }
-  
+  const thirdObj = Object.assign(secondObj, firstObj); 
+  const entries = Object.entries(thirdObj);
+  let result = {}
+  for (const [key, value] of entries) {
+    
+    if (Object.hasOwn(secondObj, key)); 
+     {
+      result[key] = 'added';
     }
-    
-    
-    return thirdObj;
-  
-  
-    
+
   }
   
-  // END
+  console.log(result)
+  return result;
+
+
+  
+}
+
+// END
