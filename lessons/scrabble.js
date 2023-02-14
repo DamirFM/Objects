9ef541be151620f952ef0a894a5757f4107160fe
Damir Yakupov <yakupovdamir93@gmail.com>
2023-02-13 22:57:34 -0500

    add scrabble.js

---
+++ lessons/scrabble.js
@@ -0,0 +1,11 @@
+
+// BEGIN (write your solution here)
+export default function scrabble(simbols, word) {
+    const isMatch = {};
+    const values = Object.values(simbols);
+    for (const value of values) {
+      const check = Object.hasOwn(isMatch, value);
+    }
+  
+  }
+  // END
